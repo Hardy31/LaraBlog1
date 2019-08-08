eca6b6a4435ccc57afc77b52023e9a470e920bdb
alex <alex-vav@list.ru>
2019-08-09 00:37:24 +0300

    add metods  in models

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+//use Cveebrock\EloquentSluggable\Sluggable;
 use Illuminate\Support\Str;  //slug
 
 
 class  Post extends Model
 {
+    //use Sluggable;
 
+    const IS_DRAFY = 0;
+    const IS_PUBLIS = 1;
 
+    protected $fields = ['title', 'content'];
 
     Public function category()
     {
@@ -35,4 +41,109 @@ class  Post extends Model
     {
         return Str::slug();
     }
+
+    public static function add($fields)
+    {
+        $post = new static;
+        $post->fill = $fields;
+        $post->user_id = 1;
+        $post->save();
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/'.$this->image);
+        $this->delete();
+    }
+
+
+     public function uploadImage($image )
+    {
+       if ($image == null) {return; }
+        Storage::delete('uploads/'.$this->image);
+        $filename = str_random(10).'.'.$image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+
+    }
+
+    public function getImage()
+    {
+        if ($this->image != null) {return '/img/no-image';}
+        return '/uploads/'. $this->image;
+    }
+
+
+    public function setCategory($id)
+    {
+        if($id == null) {return;}
+        $this->category_id = $id;
+        $this->save();
+
+    }
+
+    public function setTags ($ids)
+    {
+        if ($ids == null) {return; }
+        $this->tags()->syns($ids);
+
+    }
+
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFY;
+        $this->save();
+    }
+    public function setPublic()
+    {
+        $this->status = Post::IS_PUBLIS;
+        $this->save();
+    }
+
+    // пеоекдючатель
+    public function toggleStatus($var)
+    {
+        if ($var ==null)
+        {
+            return $this->setDraft();
+        }
+
+            return $this->setPublic();
+
+    }
+
+
+    public function setFeatured()
+    {
+        $this->is_feature = 1;
+        $this->save();
+    }
+    public function setStandart()
+    {
+        $this->is_feature = 0;
+        $this->save();
+    }
+
+
+    //пеоекдючатель
+    public function toggleFeature($var)
+    {
+        if ($var ==null)
+        {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -5,10 +5,17 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Storage;
 
-class User extends Authenticatable
+class  User extends Authenticatable
 {
     use Notifiable;
+
+    const IS_USER = 0;
+    const IS_ADMIN = 1;
+
+    const IS_BANNED = 0;
+    const IS_ACTIVE = 1;
 
     /**
      * The attributes that are mass assignable.
@@ -47,6 +54,97 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public static function add($fields)
+    {
+        $user = new static();
+        $user->fill($fields);
+        $user->password = bcrypt($fields['password']);
+        $user->save();
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+
+        $this->fill($fields);
+        $this->password = bcrypt($fields['password']);
+        $this->save();
+
+    }
+
+    public function remuv()
+    {
+        Storage::delete('uploads/'.$this->image);
+        $this->delite();
+    }
+
+
+
+    public function uploadAvatar($image )
+    {
+        if ($image == null) {return; }
+        Storage::delete('uploads/'.$this->image);
+        $filename = str_random(10).'.'.$image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+
+    }
+
+    public function getImage()
+    {
+        if ($this->image != null) {return '/img/no-avatar';}
+        return '/uploads/'. $this->image;
+    }
+
+    public function makeAdmin()
+    {
+        $this->is_admin = User::IS_ADMIN;
+        $this->save();
+     }
+    public function makeNormal()
+    {
+        $this->is_admin = User::IS_USER;
+        $this->save();
+    }
+
+//пеоекдючатель
+    public function toggleAdmin($var)
+    {
+        if ($var == null) {
+            return $this->makeNormal();
+        }
+
+        return $this->makeAdmin();
+
+    }
+
+
+
+    public function ban()
+    {
+        $this->status = User::IS_BANNED;
+        $this->save();
+    }
+    public function unban()
+    {
+        $this->status = User::IS_ACTIVE;
+        $this->save();
+    }
+
+//пеоекдючатель
+    public function toggleBan ($var)
+    {
+        if ($var == null) {
+            return $this->unban();
+        }
+
+        return $this->ban();
+
+    }
+
+
 
 
 }
